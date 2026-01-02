78277b0d8bdf7f97375e24f5b0eca53f4fe0d552
Aaron G <aaron.r.gregory@gmail.com>
2026-01-02 23:24:58 +0000

    Change site URL and base URL in config
    Updated the site URL and base URL for deployment.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://ughffs.github.io',
+  url: 'https://aaronwashere.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/aaronwashere/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
